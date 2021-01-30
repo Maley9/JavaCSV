b43039732b65df563573bcf54b7ade1a0daa6d7e
Maley9 <maley08@gmail.com>
2021-01-30 09:03:57 +0100

    JavaCSV

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        String FileName = "Exel.csv";
+        String Textread = readFile(FileName);
+        System.out.println("Text in file " + "\n" + Textread);
+        new Main().sort(FileName, "csv1.txt");
+
+        PersonAdd add = new PersonAdd();
+        add.addPerson();
+
+
+
+    }
+
+
+    void sort(String FileName, String checkFile) throws IOException{
+
+        Path path = Paths.get(FileName);
+        List<String> lines = Files.readAllLines(path);
+        Collections.sort(lines);
+        Path toPath = Paths.get(checkFile);
+        Files.write(toPath, lines);
+
+
+
+    }
+
+    public static String readFile(String FileName) {
+        File FileData = new File(FileName);
+        String read = "";
+        try {
+            Scanner skaner = new Scanner(FileData);
+
+            while (skaner.hasNextLine()) {
+
+                read = read + skaner.nextLine() + "\n";
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        return read;
+
+    }
+
+
+}
---
+++ src/com/company/PersonAdd.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class PersonAdd {
+
+    public void addPerson() {
+        String line = "";
+        System.out.println("Please type new person");
+
+        try{
+            Scanner input = new Scanner(System.in);
+            System.out.println("Type first name: ");
+            String FirstName = input.next();
+            System.out.println("Type last name: ");
+            String LastName = input.next();
+            System.out.println("Input birth year: ");
+            int BirthY = input.nextInt();
+            System.out.println("Input PESEL: ");
+            long Pesel= input.nextLong();
+
+            String newPerson = FirstName +","+ LastName +","+ BirthY +","+ Pesel;
+
+            BufferedReader reader = new BufferedReader(new FileReader("Exel.csv"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("nExel.csv")));
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.newLine();
+            }
+            writer.write(newPerson);
+            writer.newLine();
+            writer.close();
+            reader.close();
+        } catch (InputMismatchException ime) {
+            System.err.println("Invalid input");
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Error : File not found ");
+        } catch (IOException ioe) {
+            System.err.println("I/O error");
+        } finally {
+            System.out.println("nExel created");
+        }
+    }
+
+}
